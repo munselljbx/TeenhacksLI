73b9c97766cfe11c8d563865518c80f7392f4328
sanicatthedisco <jaredlenn90@gmail.com>
2018-09-30 04:23:31 -0400

    did csv abilities

--- flaskr/static/main.js
+++ flaskr/static/main.js
@@ -1,5 +1,40 @@
 var student_input = [];
 var subjects = [];
+
+var fr = new FileReader();
+
+fr.onload = function () {
+    text = fr.result;
+    lines = text.split("\n");
+    student_info = [];
+    importedSubjects = [];
+    subjectList = document.getElementById("subject-list");
+    for (var i = 1; i < lines[0].length - 2; i ++) {
+        if (lines[i] != undefined) {
+            student_info.push({
+                "id": lines[i].split(", ")[0],
+                "courses": lines[i].split(", ").slice(1)
+            });
+            for (var j = 0; j < lines[i].split(", ").slice(1).length; j ++) {
+                if (!(importedSubjects.indexOf(lines[i].split(", ").slice(1)[j]) > -1)) { //js black magic OwOno
+                    importedSubjects.push(lines[i].split(", ").slice(1)[j]);
+                    listItem = document.createElement("li");
+                    listItem.innerHTML = lines[i].split(", ").slice(1)[j];
+                    removeButton = document.createElement("button");
+                    removeButton.innerHTML = "-";
+                    removeButton.id = "remove-button";
+                    removeButton.onclick = function () {
+                        this.parentElement.parentElement.removeChild(this.parentElement);
+                    };
+                    listItem.appendChild(removeButton);
+                    subjectList.appendChild(listItem);
+                }
+            }
+        }
+    }
+
+}
+
 function submitStudents() {
   var new_id = document.getElementById("student-id").value;
   var new_courses = document.getElementById("student-courses").value;
@@ -31,6 +66,6 @@ function submitSubjects() {
 
 function submitCSV() {
     importButton = document.getElementById("importButton");
-    f = importButton.files.get(0);
-    console.log(f);
+    f = importButton.files.item(0);
+    fr.readAsText(f);
 }
