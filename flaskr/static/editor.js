df7c5f8d8c62dea974b116db83b8727bcb3acaa1
sanicatthedisco <jaredlenn90@gmail.com>
2018-09-30 05:19:32 -0400

    still working on editor

--- flaskr/static/editor.js
+++ flaskr/static/editor.js
@@ -1,10 +1,63 @@
+function canvasClick () {
+    coords = canvas.relMouseCoords(event);
+    getBox(coords.x, coords.y, grid);
+}
+
+function selectRoom() {
+    placingMode = "room";
+}
+function selectHallway() {
+    placingMode = "hall";
+}
+function selectStaircase() {
+    placingMode = "stair";
+}
+function selectEmpty() {
+    placingMode = "empty";
+}
+
+function getBox (x, y, grid) {
+    for (var i = 0; i < grid.length; i ++) {
+        for (var j = 0; j < grid[i].length; j ++) {
+            box = grid[i][j];
+            if (x > box.x && x < box.x + rectWidth && y > box.y && y < box.y + rectHeight) {
+                box.type = placingMode;
+                ctx.fillRect(box.x, box.y, box.x+rectWidth, box.y+rectHeight);
+            }
+        }
+    }
+}
+
+
+function relMouseCoords(event){
+    var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+    var currentElement = this;
+
+    do{
+        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    canvasX = event.pageX - totalOffsetX;
+    canvasY = event.pageY - totalOffsetY;
+
+    return {x:canvasX, y:canvasY}
+}
+HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
+
 window.onload = function () {
 
 canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
 
-gridX = 10;
-gridY = 10;
+placingMode = "empty";
+
+gridX = 30;
+gridY = 30;
 rectWidth = canvas.width / gridX;
 rectHeight = canvas.height / gridY;
 grid = [];
@@ -12,8 +65,9 @@ grid = [];
 for (var x = 0; x < gridX; x ++) {
     grid.push([]);
     for (var y = 0; y < gridY; y ++) {
-        grid[x].push({id: "", subjects: []});
+        grid[x].push({id: "", subjects: [], type: "empty", x: x * rectWidth, y: y * rectHeight});
         ctx.strokeStyle = "black";
+        ctx.lineWidth = 0.1;
         ctx.rect(x * rectWidth, y * rectHeight, (x*rectWidth) + rectWidth, (y*rectHeight) + rectHeight);
         ctx.stroke();
     }
