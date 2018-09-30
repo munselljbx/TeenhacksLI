35bea8542284f01ddb85122e0d5b260f765e42ef
sanicatthedisco <jaredlenn90@gmail.com>
2018-09-30 05:48:58 -0400

    Merge branch 'master' of https://github.com/munselljbx/TeenhacksLI

--- flaskr/static/editor.js
+++ flaskr/static/editor.js
@@ -1,7 +1,7 @@
-function canvasClick () {
+/*function canvasClick () {
     coords = canvas.relMouseCoords(event);
-    getBox(coords.x, coords.y, grid);
-}
+    getBox(event.pageX, event.pageY, grid);
+}*/
 
 function selectRoom() {
     placingMode = "room";
@@ -9,26 +9,23 @@ function selectRoom() {
 function selectHallway() {
     placingMode = "hall";
 }
-function selectStaircase() {
+function selectStair() {
     placingMode = "stair";
 }
 function selectEmpty() {
     placingMode = "empty";
 }
-
-function getBox (x, y, grid) {
+function autoAssign() {
     for (var i = 0; i < grid.length; i ++) {
         for (var j = 0; j < grid[i].length; j ++) {
-            box = grid[i][j];
-            if (x > box.x && x < box.x + rectWidth && y > box.y && y < box.y + rectHeight) {
-                box.type = placingMode;
-                ctx.fillRect(box.x, box.y, box.x+rectWidth, box.y+rectHeight);
-            }
+            grid[i][j].id = i.toString() + j.toString();
+            console.log(grid);
         }
     }
 }
 
 
+/*
 function relMouseCoords(event){
     var totalOffsetX = 0;
     var totalOffsetY = 0;
@@ -48,16 +45,46 @@ function relMouseCoords(event){
     return {x:canvasX, y:canvasY}
 }
 HTMLCanvasElement.prototype.relMouseCoords = relMouseCoords;
-
+*/
 window.onload = function () {
-
+    function getBox (x, y, grid) {
+        for (var i = 0; i < grid.length; i ++) {
+            for (var j = 0; j < grid[i].length; j ++) {
+                box = grid[i][j];
+                if (x > box.x && x < box.x + rectWidth && y > box.y && y < box.y + rectHeight) {
+                    grid[i][j].type = placingMode;
+                    console.log(box.x);
+                    grid[i][j].id = document.getElementById("set-id").value;
+                    if (placingMode == "room") {
+                        ctx.fillStyle = "black";
+                    }
+                    if (placingMode == "hall") {
+                        ctx.fillStyle = "blue";
+                    }
+                    if (placingMode == "stair") {
+                        ctx.fillStyle = "red";
+                    }
+                    if (placingMode == "empty") {
+                        ctx.fillStyle = "white";
+                    }
+                    ctx.fillRect(box.x, box.y, rectWidth, rectHeight);
+                }
+            }
+        }
+    }
 canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
+function on_canvas_click(ev) {
+    var x = ev.clientX - canvas.offsetLeft;
+    var y = ev.clientY - canvas.offsetTop;
+    getBox(x, y, grid);
 
+}
+canvas.addEventListener('click', on_canvas_click, false);
 placingMode = "empty";
 
-gridX = 30;
-gridY = 30;
+gridX = 10;
+gridY = 10;
 rectWidth = canvas.width / gridX;
 rectHeight = canvas.height / gridY;
 grid = [];
